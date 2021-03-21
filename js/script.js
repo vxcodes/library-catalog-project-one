a4bfafa2c166f3b130cac595650e52682b289565
Viraj Patel <virajpatel@Virajs-MacBook-Pro.local>
2021-03-20 21:21:22 -0700

    wip: second commit

--- js/script.js
+++ js/script.js
@@ -2,46 +2,85 @@ let headlines;
 const $title = $('#title');
 const $description = $('#description');
 //const $author = $('#author');
-// const $subjects = $('#subjects');
+//const $subjects = $('#subjects');
 const $covers = $('#cover');
-init();
-function init(){
-    getData();
+let $input = $('input[type="text"]');
+
+$('form').on('submit', handleGetData);
+
+function handleGetData(event){
+    event.preventDefault();
+    userInput = $input.val();
+    const promise = $.ajax({
+        url: "https://openlibrary.org/works/"+userInput+".json"
+    });
+    // SUCCESS
+    promise.then(
+    (data) => {
+        console.log(data)
+        $title.text(data.title);
+        $description.text(data.description);
+        //$author.text(data.authors["author"];)
+        // $subjects.text(data.subjects);
+        $covers.text(data.covers[0]);
+
+
+        //$description.text(data)
+    // FAILURE
+    }, 
+    (error) => {
+        console.log(error);
+    });
 };
-// note: AJAX is running asynchronously while the rest of JS is running synchronously.
-function getData(){
-    $.ajax('https://openlibrary.org/works/OL8193508W.json')
-        // SUCCESS
-        .then(
-            (data) => {
-                console.log(data)
-                $title.text(data.title);
-                $description.text(data.description);
-                //$author.text(data.authors["author"];)
-                // $subjects.text(data.subjects);
-                $covers.text(data.covers[0]);
-
-
-            //$description.text(data)
-        // FAILURE
-        }, function (error) {
-            console.log(error);
-        });
-};
-
 
 
 function render(){
-    const html = headlines.map(function(headings){
-        return`
-            <article class="card">
-                <h1>${headings.title}</h1>
-                <p>${headings.description}</p>
-                <p>${headings.covers}</p>
-            </article>
-        `;
-    });
-    $headlines.append(html);
-};
+    console.log(data)
+    $title.text(data.title);
+    $description.text(data.description);
+    //$author.text(data.authors["author"];)
+    // $subjects.text(data.subjects);
+    $covers.text(data.covers[0]);
+}
 
-render();
+render()
+// init();
+// function init(){
+//     getData();
+// };
+// note: AJAX is running asynchronously while the rest of JS is running synchronously.
+// function getData(){
+//         // SUCCESS
+//         promise.then(
+//             (data) => {
+//                 console.log(data)
+//                 $title.text(data.title);
+//                 $description.text(data.description);
+//                 //$author.text(data.authors["author"];)
+//                 // $subjects.text(data.subjects);
+//                 $covers.text(data.covers[0]);
+
+
+//             //$description.text(data)
+//         // FAILURE
+//         }, function (error) {
+//             console.log(error);
+//         });
+// };
+
+
+
+// function render(){
+//     const html = headlines.map(function(headings){
+//         return`
+//             <article class="card">
+//                 <h1>${headings.title}</h1>
+//                 <p>${headings.description}</p>
+//                 <p>${headings.covers}</p>
+//             </article>
+//         `;
+//     });
+//     $headlines.append(html);
+// };
+
+// render();
