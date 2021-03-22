7cecb0e6f181011ce28fdc1cb209b01597977683
Viraj Patel <virajpatel@Virajs-MacBook-Pro.local>
2021-03-21 17:06:52 -0700

    wip: third commit

--- js/script.js
+++ js/script.js
@@ -1,31 +1,40 @@
 let headlines;
+const $headlines = $('#headlines');
 const $title = $('#title');
 const $description = $('#description');
-//const $author = $('#author');
-//const $subjects = $('#subjects');
 const $covers = $('#cover');
+const $docs = $("#docs");
+const $author_name = $("#author_name");
+const $publish_date = $("#publish_date");
+const $publish_year = $("#publish_year");
+
 let $input = $('input[type="text"]');
 
-$('form').on('submit', handleGetData);
+
+$('form').on('submit', handleGetData)
 
 function handleGetData(event){
     event.preventDefault();
     userInput = $input.val();
     const promise = $.ajax({
-        url: "https://openlibrary.org/works/"+userInput+".json"
+        url: 'https://openlibrary.org/search.json?q='+userInput+"'",
+        url_two: 'https://openlibrary.org/search/inside.json?q='+userInput+"'"
     });
     // SUCCESS
     promise.then(
     (data) => {
         console.log(data)
-        $title.text(data.title);
-        $description.text(data.description);
-        //$author.text(data.authors["author"];)
-        // $subjects.text(data.subjects);
+        //$title.text(data.title);
+        //$docs.text(data.docs)
+        //$description.text(data.description);
+
+        for (i=0;i<10;i++){
+            $headlines.append(data.docs[i].author_name[0]);
+            $headlines.append(data.docs[i].title);
+            $headlines.append(data.docs[i].publish_date[0]);
+            $headlines.append(data.docs[i].publish_year[0]);
+        }
         $covers.text(data.covers[0]);
-
-
-        //$description.text(data)
     // FAILURE
     }, 
     (error) => {
@@ -33,17 +42,39 @@ function handleGetData(event){
     });
 };
 
+$('#secondForm').on('click','#reset', function(){
+    $(this).closest('p').remove();
+});
 
-function render(){
-    console.log(data)
-    $title.text(data.title);
-    $description.text(data.description);
-    //$author.text(data.authors["author"];)
-    // $subjects.text(data.subjects);
-    $covers.text(data.covers[0]);
+// function render(){
+
+//     $title = $('#title');
+//     //$description = $('#description');
+//     // $docs = $('#docs');
+//     $author_name = $('#author_name');
+//     $publish_date = $("#publish_date");
+//     $publish_year = $("publish_year");
+//     //$contributions = $('#contributions');
+//     //$author.text(data.authors["author"];)
+//     // $subjects.text(data.subjects);
+//     //$covers.text(data.covers[0]);
+// }
+
+// render()
+
+
+function newFunc() {
+    const html = headlines.map(function(launch) {
+        return `
+            <article class="card">
+                <h1>${launch.description}</h1>
+                <p>${launch.title}</p>
+            </article>
+        `;
+    });
+    $headlines.append(html);
 }
-
-render()
+newFunc();
 // init();
 // function init(){
 //     getData();
