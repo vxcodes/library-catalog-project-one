6e1fc6481e239ac1bc58a373c76afac3e3936cc7
Viraj Patel <virajpatel@Virajs-MacBook-Pro.local>
2021-03-23 18:38:10 -0700

    feat: completes MVP

--- js/script.js
+++ js/script.js
@@ -1,72 +1,74 @@
+
 // constant variables - data that never changes
 const url_start = 'https://openlibrary.org/search.json?q=';
 // state variables - data that changes
-//let headlines;
-
+let headlines;
+let infoDesired;
+let year = new Date();
+year = year.getFullYear();
 // caches element references - parts of the dom we need to touch
-let $input = $('input[type="text"]');
 const $headlines = $('#headlines');
 const $title = $('#title');
 const $description = $('#description');
 const $covers = $('#cover');
-const $docs = $("#docs");
-const $author_name = $("#author_name");
-const $publish_date = $("#publish_date");
-const $publish_year = $("#publish_year");
+const $docs = $('#docs');
+const $author_name = $('#author_name');
+const $publish_date = $('#publish_date');
+const $publish_year = $('#publish_year');
 const $resetBtn = $('#reset');
+const $input = $('input[type="text"]');
+const $span = $('#year');
 // event listeners - capture and respond to events (i.e. user clicks on something)
+$('form').on('submit', handleGetData);
 $resetBtn.on('click', handleReset);
-
-
-
-
 // functions - code that represents actions taken/carried out
-
-$('form').on('submit', handleGetData)
-
+$span.text(year);
 function handleGetData(event) {
     event.preventDefault();
     userInput = $input.val();
+    $input.val('');
     const promise = $.ajax({
         url: url_start + userInput + "'",
-        //url_two: 'https://openlibrary.org/search/inside.json?q=' + userInput + "'",
     });
     // SUCCESS
-    promise.then(
-        (data) => {
-            //console.log(data.docs[2].author_name[0])
-            console.log(data);
-            headlines = data;
-            render();
-            });
-            (error) => {
-                console.log(error);
-            };
-    };
-
-
-
-function render() {
-    let list = []
-    const html = headlines.docs.map(function (i) {
-        for (let i=0;i<=10;i++){
-            
-            $headlines.append(`<article class="card">
-                <p><img src="http://covers.openlibrary.org/b/isbn/${headlines.docs[i].isbn[0]}-S.jpg"/></p>
-                <p>Author: ${headlines.docs[i].author_name[0]}</p>
-                <p>Title: ${headlines.docs[i].title}</p>
-                <p>Publish Date: ${headlines.docs[i].publish_date[0]}</p>
-                <p>Publish Year: ${headlines.docs[i].publish_year[0]}</p>
-            </article>`)
-            ;
-
-        };
+    promise.then(data => {
+        // console.log(data.docs[2].author_name[0]);
+        // console.log(data);
+        headlines = data;
+        render();
     });
-    // $headlines.append(html);
-};
-
-
-
+    error => {
+        console.log(error);
+    };
+}
+function render() {
+    Object.entries(headlines).forEach(function (array) {
+        // console.log(array, 'is our item')
+        if (array.includes('docs')) {
+            // console.log(array[1])
+            infoDesired = array[1];
+            infoDesired = infoDesired.slice(0, 10);
+            console.log(infoDesired)
+        }
+    });
+    // console.log(infoDesired)
+    // if articles already exist on the DOM, remove them
+    $('article').remove();
+    infoDesired.forEach(function (book) {
+        if(book.first_publish_year === undefined) {
+            book.first_publish_year = "No data available"
+        }
+        const $article = $(`<article class="card">
+            <p><img src="http://covers.openlibrary.org/b/olid/${book.edition_key[0]}-S.jpg"/></p>
+            <p>${book.title}</p>
+            <p>${book.author_name}</p>
+            <p>${book.first_publish_year}</p>
+        </article>`);
+        $headlines.append($article);
+    });
+}
 function handleReset() {
     $('article').remove()
-};
+}
+
+
